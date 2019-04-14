96705e931de183bb31067846f6f3b10418d5d276
Nikhil Pandey <nikhilpandey.pandey9@gmail.com>
2019-04-15 03:47:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,19 @@ window.addEventListener("load", () => {
     "#50c1d2"
   ];
 
+
+  const sound_array = [
+    "sounds/bubbles.mp3",
+    "sounds/clay.mp3",
+    "sounds/confetti.mp3",
+    "sounds/glimmer.mp3",
+    "sounds/moon.mp3",
+    "sounds/ufo.mp3"
+  ];
+
   pads.forEach((pad, index) => {
     pad.addEventListener("click", function() {
-      sounds[index].currentTime = 0;
-      sounds[index].play();
+      play_sound(index);
       createBubble(index);
     });
   });
@@ -23,38 +32,32 @@ window.addEventListener("load", () => {
   document.addEventListener("keypress", function(e){
     if(e.keyCode == 65){
       console.log("A");
-      sounds[0].currentTime = 0;
-      sounds[0].play();
+      play_sound(0);
       createBubble(0);
       animate_pad(0);
     }else if(e.keyCode == 83){
       console.log("s");
-      sounds[1].currentTime = 0;
-      sounds[1].play();
+      play_sound(1);
       createBubble(1);
       animate_pad(1);
     }else if(e.keyCode == 68){
       console.log("d");
-      sounds[2].currentTime = 0;
-      sounds[2].play();
+      play_sound(2);
       createBubble(2);
       animate_pad(2);
     }else if(e.keyCode == 70){
       console.log("f");
-      sounds[3].currentTime = 0;
-      sounds[3].play();
+      play_sound(3);
       createBubble(3);
       animate_pad(3);
     }else if(e.keyCode == 71){
       console.log("g");
-      sounds[4].currentTime = 0;
-      sounds[4].play();
+      play_sound(4);
       createBubble(4);
       animate_pad(4);
     }else if(e.keyCode == 72){
       console.log("h");
-      sounds[5].currentTime = 0;
-      sounds[5].play();
+      play_sound(5);
       createBubble(5);
       animate_pad(5);
     }else{
@@ -64,6 +67,14 @@ window.addEventListener("load", () => {
 
   });
 
+
+function play_sound(ind){
+  var audio_var = sound_array[ind];
+  var audio = new Audio(audio_var);
+  audio.play();
+  console.log(audio_var);
+  
+}
 
   function animate_pad(i){
     pads[i].style.animation = `dance 0.1s ease`;
@@ -78,9 +89,10 @@ window.addEventListener("load", () => {
     const bubble = document.createElement("div");
     visual.appendChild(bubble);
     bubble.style.backgroundColor = colors[index];
-    bubble.style.animation = `jump 3s linear`;
+    bubble.style.animation = `jump 1s linear`;
     bubble.addEventListener("animationend", function() {
      visual.removeChild(this);
     });
   };
 });
+

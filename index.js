a94d14bf6e54da0053ed48fc4da65a0fd6c2d3cc
Nikhil Pandey <nikhilpandey.pandey9@gmail.com>
2019-04-20 01:22:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 window.addEventListener("load", () => {
-  const sounds = document.querySelectorAll(".sound");
+
+var record_flag = 0;
+
+//THIS IS THE RECORDING TIMELINE
+var times = [];
+var notes = [];
+
+
+
   const pads = document.querySelectorAll(".pads div");
   const visual = document.querySelector(".visual");
+  
   const colors = [
     "#f7444e",
     "#78bcc4",
@@ -31,6 +40,11 @@ window.addEventListener("load", () => {
 
 
   document.addEventListener("keypress", function(e){
+   
+    if(record_flag == 1){
+    notes.push(e.which); //push into notes array
+    times.push(e.timeStamp); // push into timestamp array
+    }
     if(e.keyCode == 65){
       play_sound(0);
       createBubble(0);
@@ -59,8 +73,8 @@ window.addEventListener("load", () => {
       console.log("none");
       
     }
-
   });
+
 
 for(let index = 0; index < sound_array.length; index++) {
 var audio_var = sound_array[index];
@@ -74,7 +88,11 @@ async function play_sound(ind){
   var audio = new Audio(audio_var);
   audio.load();
   audio.play();  
+  //console.log(times);
+  console.log(notes);
 }
+
+
 
   function animate_pad(index){
     pads[index].style.animation = `dance 0.1s ease`;
@@ -94,5 +112,64 @@ async function play_sound(ind){
      visual.removeChild(this);
     });
   };
+
+
+
+  document.getElementById("playbutton").addEventListener("click",function replay_sound(){
+    console.log("start");
+    if(times.length == 0){
+      console.log("Recording Empty");
+    }else{    
+    for (let index = 0; index < times.length; index++) {
+    myfun(times[index],notes[index]);
+    }
+  }
 });
 
+
+async function myfun(time_code,note){
+
+  if(note == 65){
+     var audio_var = sound_array[0];
+     var color_replay = 0;
+   }else if(note == 83){
+     var audio_var = sound_array[1];
+     var color_replay = 1;
+   }else if(note == 68){
+     var audio_var = sound_array[2];
+     var color_replay = 2;
+   }else if(note == 70){
+     var audio_var = sound_array[3];
+     var color_replay = 3;
+   }else if(note == 71){
+     var audio_var = sound_array[4];
+     var color_replay = 4;
+   }else if(note == 72){
+     var audio_var = sound_array[5];
+     var color_replay = 5;
+   }else{
+     console.log("none");
+   }
+
+
+  setTimeout(function myfuntion(){
+        console.log("play note"+ note);
+        var audio = new Audio(audio_var);
+        createBubble(color_replay);
+        audio.load();
+        audio.play();  
+        console.log("done");
+  }, time_code-times[0])
+}
+
+
+document.getElementById("start_record").addEventListener("click",function replay_sound(){
+  console.log("start_record");
+  record_flag = 1;
+  
+});
+document.getElementById("stop_record").addEventListener("click",function replay_sound(){
+  console.log("stop_record");
+  record_flag = 0;
+});
+});
